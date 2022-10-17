24b7db80a6e1b2cfa4eb7171675fe63262e00e46
ngamelie <ng_nhn@yahoo.ca>
2022-10-17 09:01:41 -0400

    Layout design

--- ADFSDPharma/MainWindow.xaml.cs
+++ ADFSDPharma/MainWindow.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -29,5 +30,58 @@ namespace ADFSDPharma
         {
 
         }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                this.DragMove();
+        }
+
+        private void BtnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            Storyboard sb = Resources["OpenMenu"] as Storyboard;
+            sb.Begin(LeftMenu);
+            TbxUsername.Text = "";
+            PbxPassword.Password = "";
+        }
+
+        private void BtnExit_Click(object sender, RoutedEventArgs e)
+        {
+            Environment.Exit(0);
+        }      
+
+        private void BtnOK_Click(object sender, RoutedEventArgs e)
+        {
+            string user = TbxUsername.Text;
+            string pw = PbxPassword.Password.ToString();
+            if ((user == "admin") && (pw == "123"))
+            {
+                //Admin admin = new Admin();
+                //this.Visibility = Visibility.Hidden;
+                //admin.Show();
+            }
+            else if ((user == "user") && (pw == "123"))
+            {
+                //Admin admin = new Admin();
+                //this.Visibility = Visibility.Hidden;
+                //admin.Show();
+            } 
+            else
+            {
+                //MessageBox.Show("Invalid username and password");
+            }
+
+            Admin admin = new Admin();
+            this.Visibility = Visibility.Hidden;
+            admin.Show();
+        }
+
+        private void BtnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            Storyboard sb = Resources["CloseMenu"] as Storyboard;
+            sb.Begin(LeftMenu);
+        }
+
+
     }
 }
